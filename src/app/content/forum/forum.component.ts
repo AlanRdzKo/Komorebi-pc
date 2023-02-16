b6c2169fc234433a37eed35419fb8a223297efc1
AlanRdzKo <venomtoxic@live.com>
2023-02-16 16:51:42 -0600

    Forum With Accordion
    This Commit works atleast for adding documents to the collection!

--- src/app/content/forum/forum.component.ts
+++ src/app/content/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-forum',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumComponent implements OnInit {
 
-  constructor() { }
+  form={
+    title: "",
+    usuario: "",
+    desc:""
+  }
+  coleccion: string
+  registros: any
+
+  constructor(private crud: CrudService) { 
+    this.coleccion = ""
+  }
 
   ngOnInit(): void {
+    this.coleccion = 'Forum'
+    this.crud.read(this.coleccion).then((response: any)=>{
+      this.registros = response;
+      console.log(this.registros)
+    });
+  }
+
+  agregar(): void{
+    this.form={
+      title:"",
+      usuario: "",
+      desc: "",
+    }
+  }
+
+  create(): void
+  {
+  this.coleccion = 'Forum'
+  this.crud.create(this.coleccion,this.form).then((Response:any)=>{
+    if(Response){
+      alert('Se ha enviado la informacion')
+    }
+    else{
+      alert('Ha ocurrido un error.')
+    }
+    console.log(Response)
+    location.reload()
+  })
+  }
+
+  eliminar(registro:any)
+  {
+    if (confirm('¿Desea eliminar el registro?')){
+      this.crud.delete(this.coleccion, registro).then((response:any)=>{
+        if(response){
+          alert('El registro se ha eliminado correctamente')
+        }
+        else
+        {
+          alert('Ha ocurrido un error.')
+        }
+        console.log(response)
+        location.reload()
+      })
+    }
   }
 
 }
